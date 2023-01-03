2f7c47c6888a43356861036cf617c2a9ed490614
48209-Garip-Federico <garip.federico@gmail.com>
2023-01-03 11:23:31 -0300

    Video7

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MERN-Authentication
+# https://www.youtube.com/playlist?list=PL4cUxeGkcC9g8OhpOZxNdhXggFz2lOuCT
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 
-
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -46,6 +45,27 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({email, password: hash});
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All field must be filled");
+  }
+
+  const user = await this.findOne({email});
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
 
   return user;
 };
